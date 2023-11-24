403c8d0f64b1c3f68c4301f96c2d527d1259e447
OH-GITAEK <dhrlxor1512@gmail.com>
2023-11-24 18:56:57 +0900

    5일차

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,5 +3,12 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+    @Override
+    List<Article> findAll();
 }
+
+
